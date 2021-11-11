cc579434a36b0bc926f8768dc30f7944140d377b
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2021-11-11 18:26:40 -0300

    selecting elements

---
+++ script.js
@@ -0,0 +1,18 @@
+// getElementById()
+
+// const element = document.getElementById('blog-title')
+
+// getElementByClassName()
+// const element = document.getElementsByClassName('one')
+
+// getElementTagName()
+// const element = document.getElementsByTagName('p')
+
+// querySelector()
+// const element = document.querySelector('h1')
+
+// querySelectorAll()
+const elements = document.querySelectorAll('.one')
+
+elements.forEach(el => console.log(el))
+// consigo usar o forEach com o querySelectorAll, pois não dar para usar com ByClassName()
