f454f5a0ac77ee06ed2e68f82c886d65b996d359
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2021-11-11 21:27:36 -0300

    manipulaning content

--- script.js
+++ script.js
@@ -12,7 +12,34 @@
 // const element = document.querySelector('h1')
 
 // querySelectorAll()
-const elements = document.querySelectorAll('.one')
+// const elements = document.querySelectorAll('.one')
 
-elements.forEach(el => console.log(el))
+// elements.forEach(el => console.log(el))
 // consigo usar o forEach com o querySelectorAll, pois não dar para usar com ByClassName()
+
+// Manipulando conteúdo 
+// textContent
+// const element = document.querySelector('h1')
+// element.textContent += ' Olá Luis!'
+
+// const element = document.querySelector('h1')
+// element.innerHTML = 'Olá Devs!'
+
+//  OBS: Dentro do InnerHTML(""), eu posso usar codigo em html.
+
+// value
+// const element = document.querySelector('input')
+// console.log(element.value)
+// element.value = 'outro valor'
+
+// Atributos dos elementos
+
+const header = document.querySelector('header')
+header.setAttribute('id', 'header')
+
+const headerID = document.querySelector('#header')
+
+console.log(headerID.getAttribute('class'))
+
+header.removeAttribute('id')
+header.removeAttribute('class')
